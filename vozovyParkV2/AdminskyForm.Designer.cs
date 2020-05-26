9e4c1667e354d2e7791ea09feb765325e4c455b2
insanestriker02 <65606703+insanestriker02@users.noreply.github.com>
2020-05-26 14:49:11 +0200

    Add files via upload

---
+++ vozovyParkV2/AdminskyForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace vozovyParkV2
+{
+    partial class AdminskyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.newUserBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.logoutBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(14, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jste přihlášen jako ADMIN";
+            // 
+            // newUserBtn
+            // 
+            this.newUserBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.newUserBtn.Location = new System.Drawing.Point(7, 19);
+            this.newUserBtn.Name = "newUserBtn";
+            this.newUserBtn.Size = new System.Drawing.Size(322, 58);
+            this.newUserBtn.TabIndex = 1;
+            this.newUserBtn.Text = "Založení nového uživatele";
+            this.newUserBtn.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.newUserBtn);
+            this.groupBox1.Location = new System.Drawing.Point(13, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 283);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(6, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(322, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Založení nového uživatele";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button3.Location = new System.Drawing.Point(7, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(322, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Založení nového uživatele";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button4.Location = new System.Drawing.Point(6, 211);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(322, 58);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Založení nového uživatele";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // logoutBtn
+            // 
+            this.logoutBtn.Location = new System.Drawing.Point(266, 370);
+            this.logoutBtn.Name = "logoutBtn";
+            this.logoutBtn.Size = new System.Drawing.Size(75, 23);
+            this.logoutBtn.TabIndex = 6;
+            this.logoutBtn.Text = "Odhlásit";
+            this.logoutBtn.UseVisualStyleBackColor = true;
+            // 
+            // AdminskyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 405);
+            this.Controls.Add(this.logoutBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "AdminskyForm";
+            this.Text = "AdminskyForm";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button newUserBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button logoutBtn;
+    }
+}
---
+++ vozovyParkV2/Login.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace vozovyParkV2
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            label4.Hide();
+            label5.Hide();
+            label6.Hide();
+            label7.Hide();
+        }
+
+        private void loginClick(object sender, EventArgs e)
+        {
+            SHA256 sha256 = SHA256.Create();
+            string user = uzivJmenoTxtBx.Text;
+            string pass = hesloTxtBx.Text;
+            byte[] userByte = Encoding.UTF8.GetBytes(user);
+            byte[] encryptedPass = sha256.ComputeHash(Encoding.UTF8.GetBytes(pass));
+            string path = Environment.CurrentDirectory + "\\Uzivatele" + "\\" + BitConverter.ToString(userByte).Replace("-","").ToLower() + ".txt";
+
+            if (uzivJmenoTxtBx.Text == "" || hesloTxtBx.Text == "")
+            {
+                if (uzivJmenoTxtBx.Text == "")
+                {
+                    label4.Show();
+                    label4.Text = "Zadejte uživatelské jméno";
+                }
+
+                if (hesloTxtBx.Text == "")
+                {
+                    label5.Show();
+                    label5.Text = "Zadejte heslo";
+                }
+            }
+
+            if (user == "admin")
+            {
+                Regex rx = new Regex(@"(?<=pass: )\S+");
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    //Regex rx = new Regex(@"(?<=pass: )\S+");
+                    string line;
+                    string match;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+
+                        if (rx.IsMatch(line))
+                        {
+                            if (Convert.ToString(rx.Match(line)) == BitConverter.ToString(encryptedPass).Replace("-", "").ToLower())
+                            {
+                                AdminskyForm af = new AdminskyForm();
+                                this.Hide();
+                                af.ShowDialog();
+                            }
+                        }
+                        else
+                        {
+                            label6.Text = "Zadali jste spatne heslo!";
+                            label7.Text = pass;
+                            label6.Show();
+                        }
+                    }
+                }
+            }
+
+            string misPass = "";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Regex rx = new Regex(@"(?<=pass: )\S+");
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (rx.IsMatch(line))
+                    {
+                        misPass = Convert.ToString(rx.Match(line));
+                    }
+                }
+            }
+
+            if (File.Exists(path) && (misPass == ""))
+            {
+
+            }
+            
+            //using (StreamReader sr = new StreamReader(path))
+            //{
+
+            //}
+
+            if (File.Exists(path))
+            {
+                string password = "";
+                using (StreamReader sr = new StreamReader(path))
+                {
+
+
+                    Regex rx = new Regex(@"(?<=pass: )\S+");
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (rx.IsMatch(line))
+                        {
+                            password = Convert.ToString(rx.Match(line));
+                            label5.Text = password;
+                        }
+                    }
+                    label5.Text = password;
+                    label5.Show();
+                }
+                if (BitConverter.ToString(encryptedPass).Replace("-", "").ToLower() == password)
+                {
+
+                    using (StreamWriter sw = File.AppendText(path))
+                    {
+                        sw.WriteLine(DateTime.Now);
+                    }
+                    this.Hide();
+                    UzivatelskyForm uf = new UzivatelskyForm();
+                    uf.Show();  
+                }
+            }
+
+            
+        }
+        #region TxtBxesFocused
+        private void uzivJmenoFocus(object sender, EventArgs e)
+        {
+            label4.Hide();
+            //label5.Hide();
+            
+        }
+        private void hesloFocus(object sender, EventArgs e)
+        {
+            //label4.Hide();
+            label5.Hide();
+            
+        }
+
+        #endregion
+
+        private void hesloTxtBx_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.ActiveControl = loginBtn;
+                
+            }
+        }
+
+        private void loginBtn_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                loginBtn.PerformClick();
+            }
+        }
+    }
+}
